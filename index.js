9a516a9e55ca5c688fc0534fb39976148a00c6fe
Michael Kramer <michaelrkramer@gmail.com>
2020-09-02 16:47:17 -0400

    Start

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  rules: {
+    "no-comment-console": {
+      create(context) {
+        return {
+          Program(node) {
+            node.comments
+              .filter((item) => item.type === "Line")
+              .forEach((item) => {
+                const pattern = new RegExp("^\\s?console.*");
+                if (pattern.test(item.value)) {
+                  return context.report(
+                    node,
+                    item.loc,
+                    `Did you just leave a commented 'console'?!?`
+                  );
+                }
+                return null;
+              });
+          },
+        };
+      },
+    },
+  },
+};
