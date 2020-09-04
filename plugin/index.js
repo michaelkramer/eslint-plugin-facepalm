7cc69d800193239614d44981440648d88cab0996
Michael Kramer <michaelrkramer@gmail.com>
2020-09-03 22:49:58 -0400

    Report No Useless Comments

--- plugin/index.js
+++ plugin/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
     "no-comment-console": require("./rules/no-comment-console"),
+    "no-useless-comments": require("./rules/no-useless-comments"),
   },
 };
--- plugin/rules/no-comment-console.js
+++ plugin/rules/no-comment-console.js
@@ -1,5 +1,27 @@
 module.exports = {
+  meta: {
+    type: "suggestion",
+
+    docs: {
+      description: "Code that should be removed before pushing to repo.",
+      category: "Best Practices",
+      recommended: false,
+    },
+
+    fixable: "code",
+    messages: {
+      value: "Did you just leave a commented 'console.log'?",
+    },
+  },
   create: (context) => {
+    /**
+     * Gets a fixer function to remove a given 'console.' directive.
+     * @param {ASTNode} node The directive that should be removed
+     * @returns {Function} A fixer function
+     */
+    function getFixFunction(node) {
+      return (fixer) => fixer.remove(node);
+    }
     return {
       Program(node) {
         node.comments
@@ -7,11 +29,12 @@ module.exports = {
           .forEach((item) => {
             const pattern = new RegExp("^\\s?console.*");
             if (pattern.test(item.value)) {
-              return context.report(
+              return context.report({
                 node,
-                item.loc,
-                `Did you just leave a commented 'console'?!?`
-              );
+                messageId: "value",
+                loc: item.loc,
+                fix: getFixFunction(item),
+              });
             }
             return null;
           });
